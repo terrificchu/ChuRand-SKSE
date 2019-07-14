d0c1d4428931ea7c256a255c034ce59110542741
Terrificchu <rawradise@gmail.com>
2019-07-14 13:15:27 -0400

    Add project files.

---
+++ chu-rand/lib/EventHelpers.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "skse64/PapyrusVM.h"
+#include "skse64/GameForms.h"
+#include "skse64/GameRTTI.h"
+
+#include <cinttypes>
+
+namespace EventLib
+{
+	// original function: 0x009B49C0
+	// unk148 is 0x248 see skse64 TESQuest, left over naming from skse probs
+	inline bool TESQuest_IsRunning(TESQuest* quest)
+	{
+		return (quest->unk0D8.flags & 1) != 0 && (quest->unk0D8.flags >> 7 & 1) == 0 && quest->unk148 == 0;
+	}
+
+	inline UInt64 GetVMHandleForQuest(TESQuest* quest, bool isAlias = false, uint64_t aliasIndex = 0)
+	{
+		_DMESSAGE("[DEBUG] GetVMHandleForQuest(TESQuest ptr 0x%016" PRIXPTR ")", quest);
+
+		VMClassRegistry* registry = (*g_skyrimVM)->GetClassRegistry();
+
+		UInt64 handle = 0;
+
+		if (registry && quest && TESQuest_IsRunning(quest))
+		{
+			_DMESSAGE("[DEBUG] Quest exists and is running");
+
+			if (isAlias)
+			{
+				_DMESSAGE("[DEBUG] using refAlias %u", aliasIndex);
+				BGSBaseAlias* baseAlias = nullptr;
+				if (quest->aliases.GetNthItem(aliasIndex, baseAlias))
+				{
+					const auto refAlias = DYNAMIC_CAST(baseAlias, BGSBaseAlias, BGSRefAlias);
+					if (refAlias)
+					{
+						IObjectHandlePolicy* policy = registry->GetHandlePolicy();
+						handle = policy->Create(refAlias->kTypeID, refAlias);
+
+						_DMESSAGE("[DEBUG] quest alias handle is %u", handle);
+					}
+				}
+			}
+			else
+			{
+				IObjectHandlePolicy* policy = registry->GetHandlePolicy();
+				handle = policy->Create(quest->kTypeID, quest);
+				_DMESSAGE("[DEBUG] quest handle is %u", handle);
+			}
+		}
+
+		return handle;
+	}
+}
---
+++ chu-rand/src/main.cpp
@@ -0,0 +1,412 @@
+﻿#include "common/IDebugLog.h"  // IDebugLog
+#include "skse64_common/skse_version.h"  // RUNTIME_VERSION
+#include "skse64/PluginAPI.h"  // SKSEInterface, PluginInfo
+
+#include <ShlObj.h>  // CSIDL_MYDOCUMENTS
+#include <chrono>
+#include <random>
+#include "skse64/PapyrusModEvent.h"
+#include "version.h"  // VERSION_VERSTRING, VERSION_MAJOR
+
+#include "skse64/PapyrusVM.h"
+#include "skse64/PapyrusNativeFunctions.h"
+#include "skse64/PapyrusLeveledActor.h"
+#include "skse64/PapyrusLeveledItem.h"
+#include "skse64/PapyrusWornObject.h"
+#include "skse64/GameObjects.h"
+
+#include "skse64/PapyrusCell.h"
+
+#include "skse64/GameForms.h"
+
+#include "skse64/GameReferences.h"
+#include "skse64/PapyrusGameData.h"
+#include "skse64/PapyrusDelayFunctors.h"
+
+#include "skse64/GameFormComponents.h"
+#include "skse64/GameData.h"
+#include "skse64/GameRTTI.h"
+#include "skse64/GameForms.h"
+#include "skse64/GameObjects.h"
+#include "skse64/GameReferences.h"
+#include "skse64/GameExtraData.h"
+
+#include "skse64/PapyrusEvents.h"
+#include "skse64/PapyrusArgs.h"
+#include "skse64/PapyrusEventFunctor.h"
+#include "skse64/PluginManager.h"
+#include "common/IDirectoryIterator.h"
+#include "skse64/GameAPI.h"
+#include "skse64_common/Utilities.h"
+#include "skse64/Serialization.h"
+#include "skse64_common/skse_version.h"
+#include "skse64/PapyrusEventFunctor.h"
+#include "skse64/GameEvents.h"
+#include <set>
+#include "ExamplePlugin-SKSE64-master/lib/EventFunctors.h"
+#include "ExamplePlugin-SKSE64-master/lib/EventHelpers.h"
+
+static SKSEPapyrusInterface         * g_papyrus = NULL;
+extern EventDispatcher<SKSEModCallbackEvent>	g_modCallbackEventDispatcher;
+namespace chutools
+{
+
+
+	void shufflelootlst(BGSListForm * list, VMArray<TESForm*> forms)
+	{
+		if (list)
+		{
+			for (UInt32 i = 0; i < forms.Length(); i++)
+			{
+				TESForm * form = NULL;
+				forms.Get(&form, i);
+				if (form)
+					CALL_MEMBER_FN(list, AddFormToList)(form);
+			}
+		}
+	}
+
+	void shufflelootwrld(VMClassRegistry* registry, UInt32 stackId, StaticFunctionTag* thisInput, TESObjectCELL* thisCell)
+	{
+
+
+		if (!thisCell)
+		{
+			return;
+		}
+		bool refrplcd = false;
+		std::random_device rd;
+		std::mt19937 generator(rd());
+		TESQuest * quest = DYNAMIC_CAST(LookupFormByID(118045387), TESForm, TESQuest);
+		TESForm *  tmptodel = LookupFormByID(118688224);
+		BGSListForm * todel = DYNAMIC_CAST(tmptodel, TESForm, BGSListForm);
+		CALL_MEMBER_FN(todel, RevertList)();
+		registry = (*g_skyrimVM)->GetClassRegistry();
+		UInt32	numRefs = thisCell->refData.maxSize - thisCell->refData.freeEntries;
+		DataHandler * dataHandler = DataHandler::GetSingleton();
+		//UInt32 a_refhandle = NULL;
+		_MESSAGE("PREPcompleted");
+		
+
+
+		for (UInt32 i = 0; i < numRefs; i++)
+		{
+			if (thisCell->refData.refArray[i].unk08 && thisCell->refData.refArray[i].ref)
+			{
+				refrplcd = false;
+				if (referenceUtils::GetNumReferenceAliases(&thisCell->refData.refArray[i].ref->extraData) == 0)
+				{
+					UInt8 reftype = thisCell->refData.refArray[i].ref->baseForm->formType;
+					TESObjectREFR * refToReplace = thisCell->refData.refArray[i].ref;
+					NiPoint3 pos;
+					pos.x = thisCell->refData.refArray[i].ref->pos.x + 100000;
+					pos.y = thisCell->refData.refArray[i].ref->pos.y + 100000;
+					pos.z = thisCell->refData.refArray[i].ref->pos.z + 100000;
+
+					if (reftype == 27)
+					{
+						refrplcd = true;
+						int size = dataHandler->books.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESObjectBOOK * refToPlace = NULL;
+						dataHandler->books.GetNthItem(r1, refToPlace);
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+
+					}
+					else if (reftype == 26)
+					{
+						refrplcd = true;
+						int size = dataHandler->armors.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESObjectARMO * refToPlace = NULL;
+						dataHandler->armors.GetNthItem(r1, refToPlace);
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 41)
+					{
+						refrplcd = true;
+						int size = dataHandler->weapons.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESObjectWEAP* refToPlace = NULL;
+						dataHandler->weapons.GetNthItem(r1, refToPlace);
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 30)
+					{
+						refrplcd = true;
+						int size = dataHandler->ingredients.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						IngredientItem* refToPlace = NULL;
+						while (dataHandler->ingredients.GetNthItem(r1, refToPlace) == NULL)
+						{
+							r1 = distribution(generator);
+							dataHandler->ingredients.GetNthItem(r1, refToPlace);
+						}
+						if (refToPlace != NULL)
+						{
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 32)
+					{
+						refrplcd = true;
+						int size = dataHandler->miscObjects.count - 1;
+						int r1sub = std::rand() % 10;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = 1;
+						int tsize = size - 966;
+						if (r1sub < 5)
+						{
+							std::uniform_int_distribution<int> distribution(1, 427);
+							r1 = distribution(generator);
+						}
+						else if (r1sub > 5)
+						{
+							std::uniform_int_distribution<int> distribution(967, size);
+							r1 = distribution(generator);
+						}
+						TESObjectMISC* refToPlace = NULL;
+						dataHandler->miscObjects.GetNthItem(r1, refToPlace);
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 46)
+					{
+						refrplcd = true;
+						int size = dataHandler->potions.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						AlchemyItem* refToPlace = NULL;
+						while (dataHandler->potions.GetNthItem(r1, refToPlace) == NULL)
+						{
+							int r1 = distribution(generator);
+							dataHandler->potions.GetNthItem(r1, refToPlace);
+						}
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 40)
+					{
+						refrplcd = true;
+						int size = dataHandler->arrFURN.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESFurniture* refToPlace = NULL;
+						while (dataHandler->arrFURN.GetNthItem(r1, refToPlace) == NULL)
+						{
+							int r1 = distribution(generator);
+							dataHandler->arrFURN.GetNthItem(r1, refToPlace);
+						}
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							//CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 43)
+					{
+						refrplcd = false;
+						int size = dataHandler->npcs.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESNPC * refToPlace = NULL;
+						while (dataHandler->npcs.GetNthItem(r1, refToPlace) == NULL)
+						{
+							int r1 = distribution(generator);
+							dataHandler->npcs.GetNthItem(r1, refToPlace);
+						}
+						if (refToPlace != NULL)
+						{
+							
+							Actor * actor = DYNAMIC_CAST(refToReplace, TESObjectREFR, Actor);
+							if (actor != NULL)
+							{
+								CALL_MEMBER_FN(actor, SetRace)(refToPlace->race.race, false);
+							}
+							
+							//PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							////CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else if (reftype == 61)
+					{
+						
+					}
+					else if (reftype == 28)
+					{
+					
+						refrplcd = true;
+						int size = dataHandler->arrCONT.count - 1;
+						std::uniform_int_distribution<int> distribution(1, size);
+						int r1 = distribution(generator);
+						TESObjectCONT* refToPlace = NULL;
+						while (dataHandler->arrCONT.GetNthItem(r1, refToPlace) == NULL)
+						{
+							int r1 = distribution(generator);
+							dataHandler->arrCONT.GetNthItem(r1, refToPlace);
+						}
+						if (refToPlace != NULL)
+						{
+
+							PlaceAtMe_Native(registry, stackId, thisCell->refData.refArray[i].ref, refToPlace, 1, false, false);
+							//UInt32 a_refhandle = GetOrCreateRefrHandle(thisCell->refData.refArray[i].ref);
+							//TESWorldSpace* worldspace = CALL_MEMBER_FN(thisCell->refData.refArray[i].ref, GetWorldspace)();
+							//MoveRefrToPosition(thisCell->refData.refArray[i].ref, &a_refhandle, thisCell, worldspace, &pos, &thisCell->refData.refArray[i].ref->rot);
+							////CALL_MEMBER_FN(todel, AddFormToList)(DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm));
+						}
+					}
+					else
+					{
+						/*
+							const char * john = thisCell->refData.refArray[i].ref->baseForm->GetName();
+							if (john)
+							{
+
+								_MESSAGE(john);
+							}
+							std::string p = std::to_string(thisCell->refData.refArray[i].ref->formID);
+
+								_MESSAGE(p.c_str());
+
+
+							p = std::to_string(thisCell->refData.refArray[i].ref->baseForm->formType);
+
+								_MESSAGE(p.c_str());
+
+							*/
+					}
+				}
+				if (refrplcd == true)
+				{
+					BSFixedString eventName("CHUrefFilled");
+
+
+					TESForm * form2send = DYNAMIC_CAST(thisCell->refData.refArray[i].ref, TESObjectREFR, TESForm);
+					EventQueueFunctor< TESObjectREFR *, TESForm *>(eventName, form2send).SendEvent(thisCell->refData.refArray[i].ref);
+
+					const uint64_t handle = EventLib::GetVMHandleForQuest(quest, false, 0);
+					if (handle)
+					{
+
+							EventLib::EventFunctor1<TESForm *>(eventName, form2send)(handle);
+					}
+				}
+					
+					
+				
+			}
+
+		}
+		_MESSAGE("Random succeed");
+		return;
+	}
+	bool RegisterFuncs(VMClassRegistry* a_registry) {
+		a_registry->RegisterFunction(
+			new NativeFunction1<StaticFunctionTag, void, TESObjectCELL*>("shufflelootwrld", "chutools", chutools::shufflelootwrld, a_registry));
+
+		return true;
+	}
+
+
+
+
+
+
+}
+extern "C" {
+	bool SKSEPlugin_Query(const SKSEInterface* a_skse, PluginInfo* a_info)
+	{
+		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Chustools.log");
+
+		//gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\skse64.log");
+
+		gLog.SetPrintLevel(IDebugLog::kLevel_DebugMessage);
+		gLog.SetLogLevel(IDebugLog::kLevel_DebugMessage);
+
+		_MESSAGE("Chu's Tools v%s", MYFP_VERSION_VERSTRING);
+
+		a_info->infoVersion = PluginInfo::kInfoVersion;
+		a_info->name = "Chu's Tools ";
+		a_info->version = MYFP_VERSION_MAJOR;
+
+		if (a_skse->isEditor) {
+			_FATALERROR("[FATAL ERROR] Loaded in editor, marking as incompatible!\n");
+			return false;
+		}
+		else if (a_skse->runtimeVersion != RUNTIME_VERSION_1_5_80) {
+			_FATALERROR("[FATAL ERROR] Unsupported runtime version %08X!\n", a_skse->runtimeVersion);
+			return false;
+		}
+
+		return true;
+	}
+
+
+	bool SKSEPlugin_Load(const SKSEInterface* a_skse)
+	{
+		_MESSAGE("chutoolsloaded");
+		g_papyrus = (SKSEPapyrusInterface *)a_skse->QueryInterface(kInterface_Papyrus);
+
+		//Check if the function registration was a success...
+		bool btest = g_papyrus->Register(chutools::RegisterFuncs);
+
+		if (btest) {
+			_MESSAGE("Register Succeeded");
+		}
+		return true;
+	}
+};
